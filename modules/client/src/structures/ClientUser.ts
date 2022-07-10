9abd21e8cd6b3f405f31fc2a78c7f629ce5cb76a
abdulrahman1s <61483023+abdulrahman1s@users.noreply.github.com>
2022-07-10 17:26:48 +0200

    feat(client): Relationships

--- modules/client/src/structures/ClientUser.ts
+++ modules/client/src/structures/ClientUser.ts
@@ -1,3 +1,7 @@
 import { User } from './User.ts';
+import { Collection } from '../deps.ts';
 
-export class ClientUser extends User {}
+export class ClientUser extends User {
+  readonly friends = new Collection<string, User>();
+  readonly blocked = new Collection<string, User>();
+}
--- modules/client/src/structures/User.ts
+++ modules/client/src/structures/User.ts
@@ -1,10 +1,20 @@
 import { Base, Client } from './Base.ts';
+import { DMChannel } from './mod.ts';
 import { APIUser, Badges } from '../deps.ts';
+
+export enum RelationshipStatus {
+  Friend = 0,
+  Incoming = 1,
+  Outgoing = 2,
+  Blocked = 3,
+  BlockedByOther = 4,
+}
 
 export class User extends Base {
   username!: string;
   avatar: string | null = null;
   badges = new Badges();
+  relationship!: RelationshipStatus;
 
   constructor(client: Client, data: APIUser) {
     super(client);
@@ -17,7 +27,18 @@ export class User extends Base {
     if (data.username) this.username = data.username;
     if ('avatar' in data) this.avatar = data.avatar ?? null;
     if ('badges' in data) this.badges.set(BigInt(data.badges));
+    if ('relationship' in data) this.relationship = data.relationship;
+
+    if (data.relationship == RelationshipStatus.Friend) {
+      this.client.user!.friends.set(this.id, this);
+    } else if (data.relationship == RelationshipStatus.Blocked) {
+      this.client.user!.blocked.set(this.id, this);
+    }
 
     return this;
+  }
+
+  createDM(): Promise<DMChannel> {
+    return this.client.users.createDM(this);
   }
 }
